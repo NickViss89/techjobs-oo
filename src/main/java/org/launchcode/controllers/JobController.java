cd24acfea00320b0c55902a5ad314fbfdcab1e4e
Nick Visser <nickvisser89@gmail.com>
2018-06-10 13:06:44 -0500

    finished TODOs

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -1,10 +1,13 @@
 package org.launchcode.controllers;
 
-import org.launchcode.models.forms.JobForm;
+import javafx.geometry.Pos;
+import org.launchcode.models.*;
 import org.launchcode.models.data.JobData;
+import org.launchcode.models.forms.JobForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -24,6 +27,9 @@ public class JobController {
     public String index(Model model, int id) {
 
         // TODO #1 - get the Job with the given ID and pass it into the view
+    Job job = jobData.findById(id);
+    model.addAttribute("job", job);
+
 
         return "job-detail";
     }
@@ -41,7 +47,24 @@ public class JobController {
         // new Job and add it to the jobData data store. Then
         // redirect to the job detail view for the new Job.
 
-        return "";
+        if(errors.hasErrors()) {
+            model.addAttribute(jobForm);
+            return "new-job";
+        }
+        String newName = jobForm.getName();
+        Employer newEmployer = jobData.getEmployers().findById(jobForm.getEmployerId());
+        Location newLocation = jobData.getLocations().findById(jobForm.getLocationId());
+        PositionType newPositionType = jobData.getPositionTypes().findById(jobForm.getPositionTypeId());
+        CoreCompetency newCoreCompetency = jobData.getCoreCompetencies().findById(jobForm.getCoreCompetenciesId());
+
+
+        Job newJob = new Job(newName, newEmployer, newLocation, newPositionType, newCoreCompetency);
+
+        jobData.add(newJob);
+
+        model.addAttribute(newJob);
+
+        return "job-detail";
 
     }
 }
